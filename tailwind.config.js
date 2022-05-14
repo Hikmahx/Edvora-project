c18c9e1a563544337af66aef0d43bc59b8888146
Hikmahx <hikmayousuph@gmail.com>
2022-05-13 22:26:37 -1100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      // bg
+      "nav-black": "#101010",
+      "main-black": "#292929",
+      "card-black": "#171717",
+      "button-black": "#000000",
+      "filter-black": "#131313",
+      "select-black": "#232323",
+
+      // text
+      "tab-text": "#d0cbcb",
+      "gray-6": "#f2f2f2",
+      "card-info-text": "#cfcfcf",
+      "filter-text": "#a5a5a5",
+
+      "light-grayish-blue": "#f3f4f6",
+      transparent: "transparent",
+      white: "#ffffff",
+      black: "#000000",
+    },
+    fontFamily: {
+      inter: ["Inter", "sans-serif"],
+      roboto: ["Roboto", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
