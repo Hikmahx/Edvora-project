a862c44b4e865fe2019183e28ea1dfb612c79e64
Hikmahx <hikmayousuph@gmail.com>
2022-05-15 23:25:21 -1100

    component updated

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, Fragment } from "react";
 import RideContext from "../../context/RideContext";
 import CardItem from "./CardItem";
 
@@ -7,21 +7,21 @@ const Cards = () => {
   return (
     <div className="cards px-4 sm:px-11 pb-8 mt-10 lg:mt-0">
       {!error ? (
-        <>
+        <Fragment>
           {loading ? (
             <h2 className="text-white">Loading...</h2>
           ) : (
-            <>
+            <Fragment>
               {filterRides.map((ride, index) => (
                 <CardItem key={index} ride={ride} />
               ))}
-            </>
+            </Fragment>
           )}
-        </>
+        </Fragment>
       ) : (
-        <>
+        <Fragment>
           <p className="text-center text-white">{errMsg}. Reload page</p>
-        </>
+        </Fragment>
       )}
     </div>
   );
