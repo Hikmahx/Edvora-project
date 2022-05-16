e8f68ed9883d1626058087d2d4361cb741aaa32c
Hikmahx <hikmayousuph@gmail.com>
2022-05-15 15:32:36 -1100

    rides replaced with filterRides

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -3,10 +3,10 @@ import RideContext from "../../context/RideContext";
 import CardItem from "./CardItem";
 
 const Cards = () => {
-  const { rides } = useContext(RideContext);
+  const { filterRides } = useContext(RideContext);
   return (
     <div className="cards px-4 sm:px-11 pb-8 mt-10 lg:mt-0">
-      {rides.map((ride, index)=>(
+      {filterRides.map((ride, index)=>(
         <CardItem key={index} ride={ride} />
 
       ))}
